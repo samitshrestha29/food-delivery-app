515d5e8a99dec07d8e6dd1b3d504ca9e352a5f42
samitshrestha29 <samitshrestha29@gmail.com>
2023-03-25 16:35:25 +0545

    added transform and scale in container

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,6 +13,25 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currentPageValue = 0.0;
+  final double _scaleFactor = 0.8;
+  double height = 220;
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currentPageValue = pageController.page!;
+        print("current value is _$_currentPageValue");
+      });
+    });
+  }
+
+  @override
+  dispose() {
+    pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -26,29 +45,62 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   _buidPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: const EdgeInsets.only(left: 10, right: 10),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven
-                  ? const Color(0xFF69c5df)
-                  : const Color(0xFF9294cc),
-              image: const DecorationImage(
-                fit: BoxFit.contain,
-                image: AssetImage("assets/image/food.png"),
-              )),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
+    Matrix4 matrix = Matrix4.identity();
+    if (index == _currentPageValue.floor()) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currentPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currentPageValue.floor() - 1) {
+      var currScale = 1 - (_currentPageValue - index) * (1 - _scaleFactor);
+      var currTrans = height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, height * (1 - _scaleFactor) / 2, 1);
+    }
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven
+                    ? const Color(0xFF69c5df)
+                    : const Color(0xFF9294cc),
+                image: const DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage("assets/image/food.png"),
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
               height: 120,
               margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
+                borderRadius: BorderRadius.circular(20),
                 color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                    color: Color(0xFFe8e8e8),
+                    blurRadius: 5.0,
+                    offset: Offset(5, 5),
+                  )
+                ],
               ),
               child: Container(
                 padding: const EdgeInsets.only(
@@ -78,7 +130,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                         const SizedBox(width: 10),
                         SmallText(text: "1287"),
                         const SizedBox(width: 10),
-                        SmallText(text: "comments")
+                        SmallText(text: "comments"),
                       ],
                     ),
                     const SizedBox(height: 20),
@@ -97,12 +149,14 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                             text: "32min",
                             iconColor: AppColors.iconColor2),
                       ],
-                    )
+                    ),
                   ],
                 ),
-              )),
-        ),
-      ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
